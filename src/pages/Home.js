f872ee4c1518a7cc0d03aa17dbec9fbac67fab8e
Seweryn Golba <sgoolb@gmail.com>
2023-08-03 00:47:14 +0200

    add review section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import AboutUs from "../components/AboutUs/AboutUs";
 import CompanyPros from "../components/CompanyPros/CompanyPros";
 import Hero from "../components/Hero/Hero";
 import OurCars from "../components/OurCars/OurCars";
+import Reviews from "../components/Reviews/Reviews";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <CompanyPros />
       <OurCars />
       <AboutUs />
+      <Reviews />
     </>
   );
 };
