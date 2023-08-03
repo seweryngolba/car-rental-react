2cd43774ece97d205523b22986b5307762c83b70
Seweryn Golba <sgoolb@gmail.com>
2023-08-04 01:43:06 +0200

    add carData and animation to cars

--- src/components/OurCars/OurCars.js
+++ src/components/OurCars/OurCars.js
@@ -1,57 +1,74 @@
+import React, { useState, useEffect } from "react";
 import "./OurCars.css";
-import audiPhoto from "../../assets/audir8.png";
-import kiaPhoto from "../../assets/kiastinger.png";
-import bmwPhoto from "../../assets/bmw3.png";
-import teslaPhoto from "../../assets/tesla3.png";
 import peoplePhoto from "../../assets/people.png";
 import speedPhoto from "../../assets/speed.png";
 import gasPhoto from "../../assets/gasoline.png";
 import wheelPhoto from "../../assets/wheel.png";
+import carsData from "../../data/carsData";
 
 const OurCars = () => {
+  const [activeCar, setActiveCar] = useState(carsData[0]);
+
+  useEffect(() => {
+    document.querySelector(".photoHighlight").classList.add("slideInAnimation");
+
+    setTimeout(() => {
+      document
+        .querySelector(".photoHighlight")
+        .classList.remove("slideInAnimation");
+    }, 500);
+  }, [activeCar]);
+
+  const handleCarClick = (car) => {
+    setActiveCar(car);
+  };
+
   return (
     <div className="offer">
       <h2 className="carsTitle">OUR CARS</h2>
       <div className="carsDisplay">
-        <div className="carBox active">
-          <h3 className="carName">AUDI R8</h3>
-          <img className="carPhoto audiPhoto" src={audiPhoto} alt="Audi R8" />
-        </div>
-        <div className="carBox">
-          <h3 className="carName">STINGER</h3>
-          <img className="carPhoto" src={kiaPhoto} alt="Kia Stinger" />
-        </div>
-        <div className="carBox">
-          <h3 className="carName">BMW M3</h3>
-          <img className="carPhoto" src={bmwPhoto} alt="Bmw M3" />
-        </div>
-        <div className="carBox">
-          <h3 className="carName">TESLA</h3>
-          <img className="carPhoto" src={teslaPhoto} alt="Tesla 3" />
-        </div>
+        {carsData.map((car) => (
+          <div
+            key={car.name}
+            className={`carBox ${car === activeCar ? "active" : ""}`}
+            onClick={() => handleCarClick(car)}
+          >
+            <h3 className="carName">{car.name}</h3>
+            <img className="carPhoto" src={car.photo} alt={car.name} />
+          </div>
+        ))}
       </div>
       <div className="carHighlight">
-        <img className="photoHighlight" src={kiaPhoto} alt="Car photo" />
+        <img
+          className="photoHighlight"
+          src={activeCar.photo}
+          alt={activeCar.name}
+        />
         <div className="carSpecs">
-          <h2 className="specsName">AUDI R8</h2>
+          <h2 className="specsName">{activeCar.name}</h2>
+
           <div className="specsGrid">
-            <div className="specsBox">2018</div>
+            <div className="specsBox">{activeCar.year}</div>
             <div className="specsBox">
-              <img src={peoplePhoto} alt="people" />5 People
+              <img src={peoplePhoto} alt="people" />
+              {activeCar.capacity}
             </div>
+
             <div className="specsBox">
               <img src={speedPhoto} alt="speed" />
-              240 HP
+              {activeCar.power}
             </div>
+
             <div className="specsBox">
               <img src={gasPhoto} alt="gas" />
-              Gasoline
+              {activeCar.fuel}
             </div>
+
             <div className="specsBox">
               <img src={wheelPhoto} alt="wheel" />
-              Automatic
+              {activeCar.transmission}
             </div>
-            <div className="specsBox">from 120$</div>
+            <div className="specsBox">{`from ` + activeCar.price + `$`}</div>
           </div>
           <button className="specsBtn">RENT</button>
         </div>
---
+++ src/data/carsData.js
@@ -0,0 +1,49 @@
+import audiPhoto from "../assets/audir8.png";
+import kiaPhoto from "../assets/kiastinger.png";
+import bmwPhoto from "../assets/bmw3.png";
+import teslaPhoto from "../assets/tesla3.png";
+
+const carsData = [
+  {
+    name: "AUDI R8",
+    photo: audiPhoto,
+    year: "2018",
+    capacity: "2 People",
+    power: "532 HP",
+    fuel: "Gasoline",
+    transmission: "Automatic",
+    price: 250,
+  },
+  {
+    name: "STINGER",
+    photo: kiaPhoto,
+    year: "2020",
+    capacity: "5 People",
+    power: "365 HP",
+    fuel: "Gasoline",
+    transmission: "Automatic",
+    price: 150,
+  },
+  {
+    name: "BMW M3",
+    photo: bmwPhoto,
+    year: "2017",
+    capacity: "5 People",
+    power: "480 HP",
+    fuel: "Gasoline",
+    transmission: "Manual",
+    price: 180,
+  },
+  {
+    name: "TESLA",
+    photo: teslaPhoto,
+    year: "2021",
+    capacity: "5 People",
+    power: "283 HP",
+    fuel: "Electric",
+    transmission: "Automatic",
+    price: 120,
+  },
+];
+
+export default carsData;
