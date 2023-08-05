3490224b92e3bd64b8377a7af9e204da3410dc16
Seweryn Golba <sgoolb@gmail.com>
2023-08-05 19:31:13 +0200

    add new components

---
+++ src/components/LastStep/LastStep.js
@@ -0,0 +1,13 @@
+const LastStep = () => {
+  return (
+    <div className="lastContainer">
+      <div className="summaryBox">
+        <h2 className="reservationName"></h2>
+        <div className="animationCircle"></div>
+        <h3 className="reservationSum"></h3>
+      </div>
+    </div>
+  );
+};
+
+export default LastStep;
---
+++ src/components/Payment/Payment.js
@@ -0,0 +1,10 @@
+const Payment = () => {
+  return (
+    <div>
+      <button onClick={onBack}>BACK</button>
+      <button onClick={onNext}>NEXT</button>
+    </div>
+  );
+};
+
+export default Payment;
